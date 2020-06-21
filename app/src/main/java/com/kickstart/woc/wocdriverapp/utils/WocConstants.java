8fa49506f116f884d8f10ddd3b4032f38d22eacc
Geetha <gkodandaraman@paypal.com>
2020-06-21 20:14:37 +0530

    add fixes to contactsupport

--- app/src/main/java/com/kickstart/woc/wocdriverapp/utils/WocConstants.java
+++ app/src/main/java/com/kickstart/woc/wocdriverapp/utils/WocConstants.java
@@ -9,5 +9,5 @@ public class WocConstants {
     public static final int MAP_LAYOUT_STATE_CONTRACTED = 0;
     public static final int MAP_LAYOUT_STATE_EXPANDED = 1;
     public static final int REQUEST_CALL = 1;
-    public static final String CONTACT_SUPPORT = "6505551212";
+    public static final String CONTACT_SUPPORT = "1234567890";
 }
