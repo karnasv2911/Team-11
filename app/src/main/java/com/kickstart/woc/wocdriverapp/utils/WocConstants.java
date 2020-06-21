fd43e83207f430089d9b9c5121f50b71796af194
Geetha <gkodandaraman@paypal.com>
2020-06-21 19:30:51 +0530

    fixed backstack

--- app/src/main/java/com/kickstart/woc/wocdriverapp/utils/WocConstants.java
+++ app/src/main/java/com/kickstart/woc/wocdriverapp/utils/WocConstants.java
@@ -5,7 +5,6 @@ public class WocConstants {
     public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9001;
     public static final int PERMISSIONS_REQUEST_ENABLE_GPS = 9002;
     public static final int ERROR_DIALOG_REQUEST = 9003;
-    public static final int PERMISSIONS_REQUEST_PHONE_CALL = 9004;
     public static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
     public static final int MAP_LAYOUT_STATE_CONTRACTED = 0;
     public static final int MAP_LAYOUT_STATE_EXPANDED = 1;
